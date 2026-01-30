995bc8f92a8f7148d4e454d6c00c86a3401b09d7
xjxxjx1017 <xjxxjx1017@gmail.com>
2026-01-31 12:12:28 +1300

    enemies inside ground will die

---
+++ Assets/Scripts/EnemyKillChecker.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+
+public class EnemyKillChecker : MonoBehaviour
+{
+    public GameObject enemyObject;
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Ground"))
+        {
+            // kill the enemy
+            Destroy(enemyObject);
+        }
+    }
+}
